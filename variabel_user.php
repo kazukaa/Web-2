4a0e09a205221516ca40e3f004c50d38ffdd42a7
kazukaa <yashikazukata@gmail.com>
2025-02-20 12:14:06 +0700

    praktikum 1

---
+++ variabel_user.php
@@ -0,0 +1,12 @@
+<?php
+//mendefinisikan variabel
+$nama = "Aryana";
+$umur = 18;
+$berat = 55;
+
+echo "Nama Saya " .$nama;
+echo "<br/>Umur : " .$umur . " Tahun";
+echo "<br/>Berat : " .$berat;
+
+
+?>
